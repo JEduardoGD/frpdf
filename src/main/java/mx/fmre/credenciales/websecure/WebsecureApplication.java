68218d59c72a8bbf529bbe334f9e5fbb3bddfff4
Eduardo Gutierrez <eduardo_gd@hotmail.com>
2022-04-17 08:19:56 -0500

    first commit

---
+++ src/main/java/mx/fmre/credenciales/websecure/WebsecureApplication.java
@@ -0,0 +1,13 @@
+package mx.fmre.credenciales.websecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsecureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/fmre/credenciales/websecure/WebsecureApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.fmre.credenciales.websecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
